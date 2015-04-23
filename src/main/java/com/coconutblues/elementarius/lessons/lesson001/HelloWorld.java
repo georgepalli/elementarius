692f7aff9ea88c5f79a857ace90e5bc74fa6ed31
George Abraham <george@coconutblues.com>
2015-04-22 23:12:49 -0700

    Initial commit on lessons branch

---
+++ src/main/java/com/coconutblues/elementarius/lessons/lesson001/HelloWorld.java
@@ -0,0 +1,85 @@
+package com.coconutblues.elementarius.lessons.lesson001;
+
+/**
+ * Lesson 001 - Hello World.
+ * 
+ * This lesson is designed to introduce you to Java.
+ * Implement all the methods below and run the unit tests in {@link HelloWorldTest}
+ * 
+ *
+ */
+public class HelloWorld {
+
+
+	/**
+	 * Returns <em>Hello!</em>.
+	 * 
+	 * @return a {@link String}
+	 */
+	public String sayHello() {
+		
+		// Replace the return null statement with your solution.
+		return null;
+	}
+	
+	/**
+	 * Returns <em> Hello name!</em> where name can be any value.
+	 * <br>
+	 * E.g.
+	 * <ol>
+	 *  <li>sayHello("Father") should return <em>Hello Father!</em></li>
+	 *  <li>sayHello("") should return <em>Hello!</em></li>
+	 *  <li>sayHello(null) should return <em>Hello!</em></li>
+	 * </ol> 
+	 * 
+	 * @param name
+	 * @return
+	 */
+	public String sayHello(String name) {
+		// Replace the return null statement with your solution.
+		return null;
+	}
+	
+	/**
+	 * Returns <em>Hello!</em> 10 times, one on each line.
+	 * @return
+	 */
+	public String sayHelloTenTimes(){
+		// Replace the return null statement with your solution.
+		return null;
+	}
+	
+	/**
+	 * Returns <em>Hello!</em> a number of times where the count 
+	 * is determined by the parameter numberOfTimes.
+	 * <br>
+	 * E.g.
+	 * <ol>
+	 * 	<li>sayHelloNTimes(15) should return <em>Hello!</em> 15 times, one on each line</li>
+	 * </ol>
+	 * @param numberOfTimes
+	 * @return
+	 */
+	public String sayHelloNTimes(int numberOfTimes) {
+		// Replace the return null statement with your solution.
+		return null;
+	}
+	
+	
+	/**
+	 * Returns <em>Hello name!</em> a number of times where the count 
+	 * is determined by the parameter numberOfTimes and name is any String.
+	 * <br>
+	 * E.g.
+	 * <ol>
+	 * 	<li>sayHelloNTimes(15, "Father") should return <em>Hello Father!</em> 15 times, one on each line</li>
+	 * </ol>
+	 * @param numberOfTimes
+	 * @return
+	 */
+	public String sayHelloNTimes(String name, int numberOfTimes){
+		// Replace the return null statement with your solution.
+		return null;
+	}
+
+}
---
+++ src/test/java/com/coconutblues/elementarius/lessons/lesson001/HelloWorldTest.java
@@ -0,0 +1,67 @@
+package com.coconutblues.elementarius.lessons.lesson001;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import com.coconutblues.elementarius.lessons.lesson001.HelloWorld;
+
+/**
+ * {@link HelloWorld} unit tests.
+ *
+ */
+public class HelloWorldTest {
+
+	private HelloWorld helloWorld  = new HelloWorld();
+	@Test
+	public void testSayHello() {
+		assertEquals("Hello!",helloWorld.sayHello());
+	}
+
+	@Test
+	public void testSayHelloString() {
+		assertEquals("Hello father!",helloWorld.sayHello("father"));
+		assertEquals("Hello!",helloWorld.sayHello(""));
+		assertEquals("Hello!",helloWorld.sayHello(null));
+	}
+
+	@Test
+	public void testSayHelloTenTimes() {
+		String result = getHelloNTimes(10,null);
+		assertEquals(result,helloWorld.sayHelloTenTimes());		
+	}
+
+	@Test
+	public void testSayHelloNTimes() {
+		String five = getHelloNTimes(5,null);
+		assertEquals(five,helloWorld.sayHelloNTimes(5));
+
+		String hundred = getHelloNTimes(100,null);
+		assertEquals(hundred,helloWorld.sayHelloNTimes(100));
+
+	}
+	
+	
+	@Test
+	public void testSayHelloNTimeString() {
+		String five = getHelloNTimes(5,"Mother");
+		assertEquals(five,helloWorld.sayHelloNTimes(5));
+
+		String hundred = getHelloNTimes(100,"Father");
+		assertEquals(hundred,helloWorld.sayHelloNTimes(100));
+
+	}
+
+	private String getHelloNTimes(int times, String name) {
+		StringBuilder sb = new StringBuilder();
+		for(int i=0;i<times;++i){
+			if(name == null || "".equalsIgnoreCase(name)) {
+				sb.append("Hello!");
+			} else {
+				sb.append("Hello" + name + "!");						
+			}
+			sb.append("\n");
+		}
+		return sb.toString();
+	}
+
+}
